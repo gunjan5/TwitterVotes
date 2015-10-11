0e47b6d666af8eeeabdbda6976321b0382ce60d8
Gunjan Patel <patelgunjan5@gmail.com>
2015-10-10 23:54:19 -0700

    auth and connection

--- socialpoll/twittervotes/twitter.go
+++ socialpoll/twittervotes/twitter.go
@@ -1,11 +1,45 @@
 package twitter
 
 import (
+	"github.com/joeshaw/envdecode"
+	"io"
+	"log"
 	"net"
 	"time"
 )
 
-var conn net.Conn
+var (
+	conn       net.Conn
+	reader     io.ReadCloser
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+func setupTwitterAuth() {
+	var autinfo struct {
+		ConsumerKey    string `env:"PTG_TWITTER_API_KEY,requited"`
+		ConsumerSecret string `env:"PTG_TWITTER_API_SECRET,requited"`
+		AccessToken    string `env:"PTG_TWITTER_ACCESS_TOKEN,requited"`
+		AccessSecret   string `env:"PTG_TWITTER_ACCESS_TOKEN_SECRET,requited"`
+	}
+
+	if err := envencode.Decode(&authinfo); err != nil {
+		log.Fatalln(err)
+	}
+
+	creds = &oauth.Credentials{
+		Token:  authinfo.AccessToken,
+		Secret: authinfo.AccessSecret,
+	}
+
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  authinfo.ConsumerKey,
+			Secret: authinfo.ConsumerSecret,
+		},
+	}
+
+}
 
 func dial(netw, addr string) (net.Conn, error) {
 	if conn != nil { //close the connection and set it to nil if it's not closed properly before
@@ -21,4 +55,14 @@ func dial(netw, addr string) (net.Conn, error) {
 
 	conn = netc
 	return netc, nil
+}
+
+func closeConn() {
+	if conn != nil {
+		conn.Close()
+	}
+	if reader != nil {
+		reader.Close()
+	}
+
 }
