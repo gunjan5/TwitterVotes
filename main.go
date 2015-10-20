97b6f351093ff2f4d244f84c8d1c98c7683c638b
Gunjan Patel <patelgunjan5@gmail.com>
2015-10-19 23:57:52 -0700

    options

--- main.go
+++ main.go
@@ -17,6 +17,21 @@ func closedb() {
 	log.Println("closed database connection")
 }
 
+type poll struct {
+	Options []string
+}
+
+func loadOptions() ([]string, error) {
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p) {
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
+
 func main() {
 
 }
